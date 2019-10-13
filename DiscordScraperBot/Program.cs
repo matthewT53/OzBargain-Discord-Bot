b379e61ec0995054a1dcda7e0a3e69487847c5df
Matthew <swooptank@protonmail.com>
2019-10-14 01:08:46 +1100

    Added a utility and config class to store confgiuration data

--- DiscordScraperBot/Program.cs
+++ DiscordScraperBot/Program.cs
@@ -6,7 +6,7 @@ namespace DiscordScraperBot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(Utilities.GetAlert("Test2"));
         }
     }
 }
---
+++ DiscordScraperBot/Utilities.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DiscordScraperBot
+{
+    class Utilities
+    {
+        private static Dictionary<string, string> alerts;
+        static Utilities()
+        {
+            string json = File.ReadAllText("SystemLang/alerts.json");
+            var data = JsonConvert.DeserializeObject<dynamic>(json);
+            alerts = data.ToObject<Dictionary<string, string>>();
+        }
+
+        public static string GetAlert(string key)
+        {
+            return alerts.GetValueOrDefault(key);
+        }
+    }
+}
