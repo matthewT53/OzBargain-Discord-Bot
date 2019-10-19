9b6ea0b3aba93c69754eea2858d6e3023fed883e
Matthew <Matthew@Matthew-Pc.home>
2019-10-19 18:12:26 +1100

    Did some work with the command handler

--- DiscordScraperBot/CmdHandler.cs
+++ DiscordScraperBot/CmdHandler.cs
@@ -2,7 +2,9 @@
 using Discord.WebSocket;
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace DiscordScraperBot
 {
@@ -10,5 +12,22 @@ namespace DiscordScraperBot
     {
         DiscordSocketClient _client;
         CommandService _service;
+
+        public async Task InitialiseAsync(DiscordSocketClient client)
+        {
+            _client = client;
+            _service = new CommandService();
+            await _service.AddModulesAsync(Assembly.GetEntryAssembly(), null);
+            _client.MessageReceived += HandleCommandMessage;
+        }
+
+        private async Task HandleCommandMessage(SocketMessage s)
+        {
+            var msg = s as SocketUserMessage;
+            if (msg == null) 
+                return;
+
+            var context = new SocketCommandContext(_client, msg);
+        }
     }
 }
