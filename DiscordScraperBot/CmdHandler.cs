c57359c26c8affe1b2ca272c57c6c118c48f9a1e
Matthew <Matthew@Matthew-Pc.home>
2019-10-20 01:59:12 +1100

    Finished the command handler

--- DiscordScraperBot/CmdHandler.cs
+++ DiscordScraperBot/CmdHandler.cs
@@ -18,16 +18,29 @@ namespace DiscordScraperBot
             _client = client;
             _service = new CommandService();
             await _service.AddModulesAsync(Assembly.GetEntryAssembly(), null);
-            _client.MessageReceived += HandleCommandMessage;
+            _client.MessageReceived += HandleCommandMessageAsync;
         }
 
-        private async Task HandleCommandMessage(SocketMessage s)
+        private async Task HandleCommandMessageAsync(SocketMessage s)
         {
             var msg = s as SocketUserMessage;
             if (msg == null) 
                 return;
 
             var context = new SocketCommandContext(_client, msg);
+            int argPos = 0;
+
+            // Handle the event where the user enters a command or mentions the bot.
+            // _client.CurrentUser is the bot.
+            if (msg.HasStringPrefix(Config.bot.cmdPrefix, ref argPos) 
+                || msg.HasMentionPrefix(_client.CurrentUser, ref argPos))
+            {
+                var result = await _service.ExecuteAsync(context, argPos, null);
+                if (!result.IsSuccess && result.Error != CommandError.UnknownCommand)
+                {
+                    Console.WriteLine(result.ErrorReason);
+                }
+            }
         }
     }
 }
--- DiscordScraperBot/Program.cs
+++ DiscordScraperBot/Program.cs
@@ -1,13 +1,32 @@
-﻿using System;
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
 
 namespace DiscordScraperBot
 {
     class Program
     {
+        DiscordSocketClient _client;
+        CmdHandler _cmd_handler;
         static void Main(string[] args)
+        => new Program().StartAsync().GetAwaiter().GetResult();
+
+        public async Task StartAsync()
         {
-            Console.WriteLine(Config.bot.token);
-            Console.WriteLine(Utilities.GetAlert("Test"));
+            if (Config.bot.token == "" || Config.bot.token == null) return;
+
+            _client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Verbose
+            });
+
+            _client.Log += LogMessageAsync;
+        }
+
+        private async Task LogMessageAsync(LogMessage m)
+        {
+            Console.WriteLine(m.Message);
         }
     }
 }
