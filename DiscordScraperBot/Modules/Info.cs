b67c79dd13ff1fc510412da72602b066d77c745a
Matthew <Matthew@Matthew-Pc.home>
2019-10-30 01:01:57 +1100

    Commit before sleep time

---
+++ DiscordScraperBot/Modules/Info.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Discord;
+using Discord.Commands;
+
+namespace DiscordScraperBot.Modules
+{
+    public class Info : ModuleBase<SocketCommandContext>
+    {
+
+    }
+}
